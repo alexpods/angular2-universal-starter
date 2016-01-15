d10bc56ae9b7ae74a84ca685bbcf3907127be2e8
Ben Kinsesy <bkinsey@gmail.com>
2016-01-14 22:42:36 -1000

    Fix a couple of case issues (App -> app)

--- src/boot_worker_app.ts
+++ src/boot_worker_app.ts
@@ -1,6 +1,6 @@
 import { platform } from "angular2/core";
 import { WORKER_APP_PLATFORM, WORKER_APP_APPLICATION } from "angular2/platform/worker_app";
-import { App } from './app/App';
+import { App } from './app/app';
 
 
 platform(WORKER_APP_PLATFORM).application(WORKER_APP_APPLICATION).bootstrap(App).then(() => setTimeout(() => {
--- src/server/app.ts
+++ src/server/app.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import * as express from 'express';
 import * as serveStatic from 'serve-static';
 import { ng2engine } from  'angular2-universal-preview';
-import { App } from '../app/App.ts';
+import { App } from '../app/app.ts';
 
 const PUBLIC_PATH = path.resolve(__dirname, '../../dist/public');
 const INDEX_PATH  = path.resolve(__dirname, '../index.html');
