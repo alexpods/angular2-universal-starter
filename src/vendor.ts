9fc31c870fc2e551a3b807ffb83b45e0d0fe1967
alexpods <aleksey.podskrebyshev@gmail.com>
2016-01-18 14:42:37 +0300

    Update angular2-universal-preview

--- src/vendor.ts
+++ src/vendor.ts
@@ -3,5 +3,5 @@ import 'es6-promise';
 import 'reflect-metadata';
 import 'zone.js/lib/browser/zone-microtask';
 import 'zone.js/lib/browser/long-stack-trace-zone';
-import 'rxjs';
 import 'angular2/core';
+import 'angular2/router';
