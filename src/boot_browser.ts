3f508d4e2a1510adf19ac35c0fa198b77ff0963f
alexpods <aleksey.podskrebyshev@gmail.com>
2016-08-27 23:48:31 +0800

    Fixed the bug with 'Cannot find module '@angular/router'

--- src/boot_browser.ts
+++ src/boot_browser.ts
@@ -1,7 +1,6 @@
 import './polyfills';
 
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 
 bootstrap(App, []);
--- src/boot_worker_app.ts
+++ src/boot_worker_app.ts
@@ -1,7 +1,6 @@
 import './polyfills';
 
 import { bootstrapWorkerApp } from '@angular/platform-browser-dynamic';
-import { provideRouter } from '@angular/router';
 import { App } from './app/app';
 
 bootstrapWorkerApp(App, []);
